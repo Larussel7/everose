4cede0b7d63753408e15bf346c6e3bddaca9646f
Anton <antonputra@icloud.com>
2017-02-17 14:01:45 -0500

    roots changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'line_items/index'
+  #temp root
+  root "products#index"
 
-  get 'line_items/show'
-
-  get 'line_items/new'
-
-  get 'line_items/edit'
+  resources :line_items
 
   resources :products do
     member do
       get :delete
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
