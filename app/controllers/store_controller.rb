4a1881eed1da24f526f98d4b0a559455a65492f4
Anton <antonputra@icloud.com>
2017-02-19 11:17:39 -0500

    fixed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -17,6 +17,7 @@ class StoreController < ApplicationController
 
   def buket
     @product = Product.find(params[:id])
+
     if session[:cart_id]
       ses1 = session[:cart_id]
       @path = cart_path(ses1)
