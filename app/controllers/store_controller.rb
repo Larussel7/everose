0584e7852751b8f063ad92774dd48b97e315407d
Anton <antonputra@icloud.com>
2017-02-19 02:05:37 -0500

    if session nill then equal 1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -17,6 +17,12 @@ class StoreController < ApplicationController
 
   def buket
     @product = Product.find(params[:id])
+    if session[:cart_id]
+      ses1 = session[:cart_id]
+      @path = cart_path(ses1)
+    else
+      @path = carts_path
+    end
   end
 
   def update
